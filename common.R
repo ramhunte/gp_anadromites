245bb11e8f691870bcd6a13332af736185c96279
ramhunte <ramhunte@ucsb.edu>
2023-10-24 12:44:37 -0700

    ray updates

---
+++ common.R
@@ -0,0 +1,3 @@
+flow_stl <- st_read(here("data", "Flowline_STL", "Flowline_STL_20220928.shp"))
+flood_stl <- st_read(here("data", "Floodplain_STL", "Floodplain_STL_20220928.shp"))
+subs_stl <- st_read(here("data", "Subbasins_STL", "Subbasins_STL_20220928.shp"))
